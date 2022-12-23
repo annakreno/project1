edcc8223160ccb597cdeff8055e8951142757e6d
annakreno <annakreno@gmail.com>
2022-12-23 15:50:01 -0500

    added html and css

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # project1
 HTML/CSS/JS browser based game
+
+
+"tells the reader how to get this project running and what it does"
---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*----- constants -----*/
+
+const WIN_COMBOS = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [2, 2, 2],
+]
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+const buttonEl = document.querySelector("button")
+const slot1El = document.getElementById("slot1")
+const slot
+
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
