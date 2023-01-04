46dd5fa2b1813116ae66d4bc57d76f129e9d68fe
annakreno <annakreno@gmail.com>
2023-01-04 17:18:49 -0600

    overhaul css

--- js/main.js
+++ js/main.js
@@ -4,32 +4,50 @@ const iconImages = [
     {name: "diamond", html: "<img class='icons' id='diamond' src='slot_machine_images/Diamond.png'/>"},
     {name: "cherry", html: "<img class='icons' id='cherry' src='slot_machine_images/cherry3.png'/>"},
     {name: "bell", html: "<img class='icons' id='bell' src='slot_machine_images/bell4.png'/>"},
+    {name: "seven", html: "<img class='icons' id='seven' src='slot_machine_images/lucky_seven.png'/>"},
+    {name: "banana", html: "<img class='icons' id='banana' src='slot_machine_images/banana.png'/>"},
+    {name: "clover", html: "<img class='icons' id='clover' src='slot_machine_images/clover2.png'/>"},
+    {name: "horseshoe", html: "<img class='icons' id='horseshoe' src='slot_machine_images/horseshoe.png'/>"},
+    {name: "coin", html: "<img class='icons' id='coin' src='slot_machine_images/coin.png'/>"},
 ]
 
 /*----- app's state (variables) -----*/
 
 /*----- cached element references -----*/
-
-const buttonEl = document.querySelector("button");
 const allSlots = document.querySelectorAll(".container > div");
 const slotEl1 = document.getElementById("slot1");
 const slotEl2 = document.getElementById("slot2");
 const slotEl3 = document.getElementById("slot3");
-let winner = document.querySelector("h3");
+let winner = document.querySelector("div.winStatus");
+let leverEl = document.getElementById("lever")
 
 /*----- event listeners -----*/
-buttonEl.addEventListener("click", init);
+leverEl.addEventListener("click", init);
 
 /*----- functions -----*/
 
 function init() {
-    updateIcons()
-    checkForWinner()
+    leverPull();
+    spinSlots();
+    setTimeout(function() {updateIcons()}, 4000);
+    setTimeout(function(){checkForWinner()}, 5000);
+}
 
+function leverPull() {
+    winner.innerText = "";
+    setTimeout(function(){leverEl.innerHTML = "<img src='slot_machine_images/leverDown.png'>"}, 500);
+    setTimeout(function(){leverEl.innerHTML = "<img src='slot_machine_images/leverUp.png'>"}, 1000);
+}
+
+function spinSlots() {
+    slotEl1.innerHTML = "<img class='spinningSlot1' src='slot_machine_images/midSpin1.png'>";
+    slotEl2.innerHTML = "<img class='spinningSlot2' src='slot_machine_images/midSpin2.png'>";
+    slotEl3.innerHTML = "<img class='spinningSlot3' src='slot_machine_images/midSpin3.png'>";
+    
 }
 
 function getRandomNum() {
-    return Math.floor(Math.random() * 3);
+    return Math.floor(Math.random() * 8);
 }
 
 function updateIcons() {
@@ -41,8 +59,12 @@ function updateIcons() {
 
 function checkForWinner(allSlots) {
     if(slotEl1.innerHTML === slotEl2.innerHTML && slotEl2.innerHTML === slotEl3.innerHTML) {
-        winner.innerText = "You Won!" 
+        setTimeout(function(){
+            winner.innerHTML = "<h3> WINNER 🎉 </h3>"
+        }, 500);
     } else {
-        winner.innerText = "You Lost :( - click button to play again"
+        setTimeout(function(){
+            winner.innerHTML = "<h3> You lose 😢 </h3>"
+        }, 500);
     }
 }
