beae68216c1ae0050f4aa7065dca01e26bd025e6
annakreno <annakreno@gmail.com>
2023-01-05 18:39:06 -0600

    deploy final draft

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # project1
 HTML/CSS/JS browser based game
 
+Slot Machine: Click the lever to spin the slots. If all three slots land on the same icon, you win! If the icons don't match, you lose. Each win adds $100 to your total winnings.
 
-"tells the reader how to get this project running and what it does"
+Screenshots of game:
+![starting screen](slot_machine_images/begin_screen.png)
+![player hits the jackpot](slot_machine_images/win_screen.png)
+![player loses](slot_machine_images/lose_screen.png)
+
+Technologies Used: JavaScript, HTML, CSS.
+
+Getting Started: 
+    - https://annakreno.github.io/project1/ 
+    - Click the lever to spin the slots.
+
+Next Steps: 
+    - Add a "bomb" jackpot that results in losing all winnings
+    - Set different prize amounts for different icon jackpots.
+    - Look into random number generator, doesn't seem to land on jackpot the statistically expected number of times, lowered the options to return so that it'd land on jackpot more easily for demo purposees.
+    - Smooth the transition between spinning slot and the landed upon icon.
--- js/main.js
+++ js/main.js
@@ -23,6 +23,10 @@ let leverEl = document.getElementById("lever");
 let confettiEl = document.getElementById("confetti");
 let loseEffectEl = document.getElementById("loseEffect");
 const winningsEl = document.querySelector(".points > h3:last-child");
+const leverSoundEl = document.getElementById("leverSound");
+const spinSoundEl = document.getElementById("spinSound");
+const winSoundEl = document.getElementById("winSound");
+const loseSoundEl = document.getElementById("loseSound");
 
 /*----- event listeners -----*/
 leverEl.addEventListener("click", init);
@@ -37,12 +41,14 @@ function init() {
 }
 
 function leverPull() {
+    leverSoundEl.play();
     winner.innerText = "";
     setTimeout(function(){leverEl.innerHTML = "<img src='slot_machine_images/leverDown.png'>"}, 500);
     setTimeout(function(){leverEl.innerHTML = "<img src='slot_machine_images/leverUp.png'>"}, 1000);
 }
 
 function spinSlots() {
+    spinSoundEl.play();
     slotEl1.innerHTML = "<img class='spinningSlot1' src='slot_machine_images/midSpin1.png'>";
     slotEl2.innerHTML = "<img class='spinningSlot2' src='slot_machine_images/midSpin2.png'>";
     slotEl3.innerHTML = "<img class='spinningSlot3' src='slot_machine_images/midSpin3.png'>";
@@ -50,7 +56,7 @@ function spinSlots() {
 }
 
 function getRandomNum() {
-    return Math.floor(Math.random() * 8);
+    return Math.floor(Math.random() * 2);
 }
 
 function updateIcons() {
@@ -62,6 +68,7 @@ function updateIcons() {
 
 function checkForWinner(allSlots) {
     if(slotEl1.innerHTML === slotEl2.innerHTML && slotEl2.innerHTML === slotEl3.innerHTML) {
+        winSoundEl.play();
         confettiEl.classList.add("confetti");
         setTimeout(function(){
             winner.innerHTML = "<h3> WINNER 🎉 </h3>";
@@ -75,14 +82,16 @@ function checkForWinner(allSlots) {
             }
         setTimeout(function() {
             confettiEl.classList.remove("confetti");
-        }, 5000);
+            winner.innerHTML = "<h3> CLICK THE LEVER TO PLAY AGAIN </h3>";
+        }, 6750);
     } else {
+        loseSoundEl.play();
         setTimeout(function(){
             loseEffectEl.classList.add("loseEffect");
             winner.innerHTML = "<h3> CLICK THE LEVER TO PLAY AGAIN </h3>";
         }, 500);
         setTimeout(function() {
             loseEffectEl.classList.remove("loseEffect");
-        }, 3000);
+        }, 2000);
     }
 }
